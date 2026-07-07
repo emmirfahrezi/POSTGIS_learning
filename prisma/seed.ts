33a1afd03d5c7863b5d4aad8b68d82d1a76431d4
emmirfahrezi <emmir.fahrezi1@gmail.com>
2026-07-07 11:21:39 +0700

    feat: init project with nestjs, prisma, and bun

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  console.log('Seeding database...');
+
+  const user1 = await prisma.user.upsert({
+    where: { email: 'admin@example.com' },
+    update: {},
+    create: {
+      email: 'admin@example.com',
+      name: 'Admin User',
+      password: 'adminpassword123',
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'user@example.com' },
+    update: {},
+    create: {
+      email: 'user@example.com',
+      name: 'Regular User',
+      password: 'userpassword123',
+    },
+  });
+
+  console.log('Seeding finished.');
+  console.log({ user1, user2 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
